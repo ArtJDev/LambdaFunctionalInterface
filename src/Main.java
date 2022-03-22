1273b68118e6267bc568e79cd15298456db8d0f9
ArtJDev <artemkurilenko1983@yandex.ru>
2022-03-22 10:33:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) throws ArithmeticException {
+        Calculator calc = Calculator.instance.get();
+        int a, b, c;
+
+            a = calc.plus.apply(1, 2);
+            b = calc.minus.apply(1, 1);
+        try {
+            c = calc.devide.apply(a, b);
+        } catch (ArithmeticException ex) {
+            c = 0;
+        }
+
+        calc.println.accept(a);
+        calc.println.accept(b);
+        calc.println.accept(c);
+    }
+}
